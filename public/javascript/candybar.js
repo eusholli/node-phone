a4f5234c347fba37fae08f46f5eaf568650c3a03
Geoff Hollingworth <geoff.hollingworth@ericsson.com>
2013-01-04 12:32:47 -0800

    candybar and dialer integrated

--- public/javascript/candybar.js
+++ public/javascript/candybar.js
@@ -29,10 +29,9 @@
     return buf.join("");
 }
 
-  var phoney = window.att && window.att.phoneNumber || window.phoney;
+  var phoney = window.ATT && window.ATT.phoneNumber || window.phoney;
 
   var CandyBar = function (options) {
-
     var spec = options || {};
     this.states = {
       incoming: {
@@ -84,10 +83,6 @@
     }
   };
   
-  CandyBar.prototype.cancel = function() {
-    alert("Cancel pressed");
-  };
-
   CandyBar.prototype.render = function () {
     if (!this.dom) {
       this.dom = this.domify(template(this));
@@ -96,7 +91,7 @@
     } else {
       this.dom.innerHTML = this.domify(template(this)).innerHTML;
     }
-    this.clearUser();
+    this.setState('inactive');
     return this.dom;
   };
   
@@ -161,7 +156,7 @@
 
       // reset user if relevant
       if (stateDef.clearUser) {
-        this.setUser({});
+        this.clearUser();
       }
 
     } else {
@@ -181,6 +176,24 @@
       }, 1000);
     }, 1000);
     return this;
+  };
+
+  CandyBar.prototype.setImageUrl = function (url) {
+    this.attachImageDom(!!url);
+    this.imageDom.src = url;
+    this.dom.classList[!!url ? 'add' : 'remove']('havatar');
+  };
+
+  CandyBar.prototype.attachImageDom = function (bool) {
+    if (!this.imageDom) {
+      this.imageDom = this.dom.querySelector('.callerAvatar');
+    }
+    if (bool && !this.imageDom.parentElement) {
+      this.dom.insertBefore(this.imageDom, this.dom.firstChild);
+    } else if (this.imageDom.parentElement) {
+      this.imageDom.parentElement.removeChild(this.imageDom);
+    }
+    return this.imageDom;
   };
 
   CandyBar.prototype.registerPhoneHandlers = function (phone) {
@@ -211,15 +224,36 @@
         self.call.end && self.call.end();
         delete self.call;
       }
+    };
+    self.answer = function () {
+      if (self.call) {
+        self.call.answer();
+      }
+    };
+    self.cancel = function () {
+      if (self.call) {
+        self.call.hangup();
+      }
     }
   };
 
   CandyBar.prototype.getUser = function () {
-    var user = this.user || {};
+    var user = this.user || {},
+      self = this;
     return {
       picUrl: user.picUrl,
       name: (user.name && user.name) || this.config.defaultName,
-      number: (user.number && escape(user.number)) || this.config.defaultNumber
+      number: function () {
+        if (user.number && user.number !== self.config.defaultNumber) {
+          if (phoney) {
+            return phoney.stringify(user.number);
+          } else {
+            return escape(user.number);
+          }
+        } else {
+          return self.config.defaultNumber;
+        }
+      }()
     };
   };
 
@@ -227,16 +261,9 @@
     this.user = details;
     if (!this.dom) return;
     var user = this.getUser();
-    if(user.picUrl) {
-      this.dom.querySelector('.callerAvatar').src = user.picUrl;
-    }
-    this.dom.querySelector('.callerNumber').innerHTML = phoney.stringify(user.number);
+    this.dom.querySelector('.callerNumber').innerHTML = user.number;
     this.dom.querySelector('.callerName').innerHTML = user.name;
-    if (user.picUrl) {
-      this.dom.classList.add('havatar');
-    } else {
-      this.dom.classList.remove('havatar');
-    }
+    this.setImageUrl(user.picUrl);
     return this;
   };
 
